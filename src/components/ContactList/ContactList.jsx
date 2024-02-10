2d1dd8a2189b1a968af577c2e78be89d99753faf
Vasyl Diachuk <djachukvasja@gmail.com>
2024-02-10 16:19:48 +0200

    done

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,22 @@
+import * as Yup from 'yup';
+import css from './ContactList.module.css';
+import { Contact } from './Contact/Contact';
+
+export const ContactList = ({ contacts, handleClick }) => {
+  return (
+    <ul className={css.list}>
+      {contacts.map(({ id, name: names, number }) => {
+        return (
+          <li className={css.item} key={id}>
+            <Contact
+              id={id}
+              names={names}
+              number={number}
+              handleClick={handleClick}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
